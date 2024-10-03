703975dcb10501b66204e497e2a44bd36c877c2d
jan-seon <timothyzhong2@gmail.com>
2024-10-02 20:45:01 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "5d7d3a08-dcfc-4626-b175-c314386cf982";
+export const MODAL_CLOSE_SEC = 2.5;
